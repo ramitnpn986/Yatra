3f380baa26da84ade8c67c4580c13832e75b6b4d
bibekpandey999 <Ramitnpn986@gmail.com>
2026-09-24 12:50:05 +0545

    cloudinary error fixed

--- backend/index.ts
+++ backend/index.ts
@@ -1,40 +1,38 @@
-import dotenv from 'dotenv'
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import cookieParser from 'cookie-parser'
-import connectDB from './src/utils/db.js'
-import CutomerRoutes from './src/routes/customer.route.js'
-import TransportRoutes from './src/routes/transporter.route.js'
-import AdminRoutes from './src/routes/admin.route.js'
-dotenv.config()
+import "dotenv/config";
 
-const PORT = process.env.PORT || 5000;
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
+import connectDB from "./src/utils/db.js";
+import CutomerRoutes from "./src/routes/customer.route.js";
+import TransportRoutes from "./src/routes/transporter.route.js";
+import AdminRoutes from "./src/routes/admin.route.js";
 
-const app = express()
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
 
-app.use("/api/v88/admin-end",AdminRoutes)
-app.use("/api/v8/users",CutomerRoutes);
-app.use("/api/v8/transporters",TransportRoutes)
-
+app.use("/api/v88/admin-end", AdminRoutes);
+app.use("/api/v8/users", CutomerRoutes);
+app.use("/api/v8/transporters", TransportRoutes);
 
 const startServer = async () => {
     try {
         await connectDB();
-        app.listen(PORT, () => {
-            console.log(`server is running at port ${PORT}`)
-        })
 
+        app.listen(PORT, () => {
+            console.log(`server is running at port ${PORT}`);
+        });
     } catch (error) {
         console.error("Failed to start server:", error);
         process.exit(1);
     }
-}
+};
 
 startServer();
-
--- backend/src/config/cloudinary.ts
+++ backend/src/config/cloudinary.ts
@@ -1,9 +1,13 @@
-import { v2 as cloudinary} from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
+
+console.log("ENV CLOUD NAME:", process.env.CLOUDINARY_CLOUD_NAME);
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+});
+
+console.log("CLOUDINARY CONFIG:", cloudinary.config());
 
 export default cloudinary;
